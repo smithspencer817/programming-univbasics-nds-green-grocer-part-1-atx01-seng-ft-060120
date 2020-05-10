9cdd610ef9736b8dd64dd6e7042946f0ca1b3528
Spencer Smith <smithspencer817@gmail.com>
2020-05-10 01:30:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,9 @@ def consolidate_cart(cart)
     if new_cart_item != nil
       new_cart_item[:count] += 1
     else
-      new_cart_item = {}
+      new_cart_item = {
+        :item => 
+      }
     end
     counter += 1
   end
