dc71cf402290217a17f108a5894ccafa20378b30
Spencer Smith <smithspencer817@gmail.com>
2020-05-10 00:49:23 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,7 @@ require "pry"
 def find_item_by_name_in_collection(name, collection)
   
   collection.each do |item|
-    if item[:name] == name
+    if item[:item] == name
       return item
     end
   end
