1231a7f1208d01b06b30fc5743fa98531139d243
Spencer Smith <smithspencer817@gmail.com>
2020-05-10 01:21:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,8 +13,8 @@ end
 def consolidate_cart(cart)
   result = []
   cart.each do |item|
-    item[:item].each do
-      if result.include? (item[:item])
+    item[:item].each do |name|
+      if result.include? (name)
         item[:price] += item[:price]
         item[:count] += 1
       else        
