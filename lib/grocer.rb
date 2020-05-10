823cde046a7cdaf58e4d69a3ddab246bf49ad277
Spencer Smith <smithspencer817@gmail.com>
2020-05-10 00:44:54 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,5 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  
   collection.each do |item|
     if item[:name] == name
       return item
